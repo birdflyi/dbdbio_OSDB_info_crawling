3bee571a21cc373eb41b38c487ffef3851a30f8b
zhlou <zhlou@stu.ecnu.edu.cn>
2023-02-24 16:50:33 +0800

    Update: Update data crawling from dbdb.io in 202302

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ RECALC_OSDB_LIST = True  # Add "Name" column
 RECALC_OSDB_INFO = True  # Check "Name" column; Representing "Data Model" "Source Code" "Start Year" "End Year" columns.
 JOIN_OSDB_SUMMARY_INFO_ON_NAME = True  # join OSDB summary and OSDB_infos on filed 'Name' and 'Name'
 
-month_yyyyMM = "202301"
+month_yyyyMM = "202302"
 
 
 def get_last_month_yyyyMM(curr_month_yyyyMM):
