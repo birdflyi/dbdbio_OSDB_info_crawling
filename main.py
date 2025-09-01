1a4e80e6a0af431b0c9fbd97732d0fbf78f2a159
birdflyi <1065168006@qq.com>
2025-09-01 20:41:53 +0800

    * Refactor and Update Data: Rename a variable; Update crawled data and manulabeled data up to 20250831.

--- main.py
+++ main.py
@@ -36,27 +36,27 @@ RECALC_OSDB_LIST = True  # Add "Name" column
 RECALC_OSDB_INFO = True  # Check "Name" column; Representing "Data Model" "Source Code" "Start Year" "End Year" columns.
 JOIN_OSDB_SUMMARY_INFO_ON_NAME = True  # join OSDB summary and OSDB_infos on filed 'Name' and 'Name'
 
-month_yyyyMM = "202507"
+month_YYYYmm = "202508"
 
 
-def get_last_month_yyyyMM(curr_month_yyyyMM, interval=1):
-    curr_month_yyyyMM = str(curr_month_yyyyMM)
-    assert(curr_month_yyyyMM.isdecimal() and len(curr_month_yyyyMM) == 6)
+def get_last_month_YYYYmm(curr_month_YYYYmm, interval=1):
+    curr_month_YYYYmm = str(curr_month_YYYYmm)
+    assert(curr_month_YYYYmm.isdecimal() and len(curr_month_YYYYmm) == 6)
     from datetime import datetime
     from dateutil.relativedelta import relativedelta
 
-    curr_date_obj = datetime.strptime(curr_month_yyyyMM, '%Y%m')
+    curr_date_obj = datetime.strptime(curr_month_YYYYmm, '%Y%m')
     int_months_ago = curr_date_obj - relativedelta(months=interval)
     return int_months_ago.strftime("%Y%m")
 
 
-last_month_yyyyMM = get_last_month_yyyyMM(month_yyyyMM)
-# last_month_yyyyMM = get_last_month_yyyyMM(last_month_yyyyMM)
-src_OSDB_info_joined_last_month_manulabeled_path = os.path.join(pkg_rootdir, f'data/manulabeled/OSDB_info_{last_month_yyyyMM}_joined_manulabeled.csv')
-OSDB_crawling_path = os.path.join(pkg_rootdir, f'data/dbdbio_OSDB_list/OSDB_crawling_{month_yyyyMM}_raw.csv')
-OSDB_info_crawling_path = os.path.join(pkg_rootdir, f'data/dbdbio_OSDB_list/OSDB_info_crawling_{month_yyyyMM}_raw.csv')
-OSDB_info_joined_path = os.path.join(pkg_rootdir, f'data/dbdbio_OSDB_list/OSDB_info_{month_yyyyMM}_joined.csv')
-OSDB_info_joined_manulabeled_path = os.path.join(pkg_rootdir, f'data/manulabeled/OSDB_info_{month_yyyyMM}_joined_manulabeled.csv')
+last_month_YYYYmm = get_last_month_YYYYmm(month_YYYYmm)
+# last_month_YYYYmm = get_last_month_YYYYmm(last_month_YYYYmm)
+src_OSDB_info_joined_last_month_manulabeled_path = os.path.join(pkg_rootdir, f'data/manulabeled/OSDB_info_{last_month_YYYYmm}_joined_manulabeled.csv')
+OSDB_crawling_path = os.path.join(pkg_rootdir, f'data/dbdbio_OSDB_list/OSDB_crawling_{month_YYYYmm}_raw.csv')
+OSDB_info_crawling_path = os.path.join(pkg_rootdir, f'data/dbdbio_OSDB_list/OSDB_info_crawling_{month_YYYYmm}_raw.csv')
+OSDB_info_joined_path = os.path.join(pkg_rootdir, f'data/dbdbio_OSDB_list/OSDB_info_{month_YYYYmm}_joined.csv')
+OSDB_info_joined_manulabeled_path = os.path.join(pkg_rootdir, f'data/manulabeled/OSDB_info_{month_YYYYmm}_joined_manulabeled.csv')
 
 encoding = 'utf-8'
 
